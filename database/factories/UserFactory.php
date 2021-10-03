877998b0e1996aa8599a17c268671ad7e218bc6a
Matt MARTIN <codingstuff91@gmail.com>
2021-10-03 21:32:35 +0200

    Update UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,6 +24,8 @@ class UserFactory extends Factory
     {
         return [
             'name' => $this->faker->name(),
+            'pseudo' => $this->faker->name(),
+            'role' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
