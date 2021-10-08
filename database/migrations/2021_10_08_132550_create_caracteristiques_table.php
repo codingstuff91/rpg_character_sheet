68290598c3bfc463537e8f1176ebf76671c490e5
Matthieu MARTIN <codingstuff91@gmail.com>
2021-10-08 15:55:26 +0200

    Creation Model Caracteristique et migration associée

---
+++ database/migrations/2021_10_08_132550_create_caracteristiques_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCaracteristiquesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('caracteristiques', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('categorie_id')->constrained('categories');
+            $table->string('nom');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('caracteristiques');
+    }
+}
