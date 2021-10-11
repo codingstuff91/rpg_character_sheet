5adcef88a3fb8814c17051b200f6381737b66484
Matt MARTIN <codingstuff91@gmail.com>
2021-10-11 23:16:50 +0200

    Suppression bouton et ajout alignement textes

--- resources/js/components/VTM_V20_character_sheet.vue
+++ resources/js/components/VTM_V20_character_sheet.vue
@@ -2,19 +2,17 @@
     <div class="container is-widescreen">
         <Navbar/>
         <div class="row justify-content-center">
-            <b-button type="is-primary">Primary</b-button>
-
             <b-tabs>
                 <b-tab-item label="Attributs">
                     <div class="columns is-mobile">
-                        <div class="column is-vcentered">
-                            <h2>Physiques</h2>
+                        <div class="column is-4">
+                            <h2 class="has-text-centered">Physiques</h2>
                         </div>
-                        <div class="column is-vcentered">
-                            <h2>Sociaux</h2>
+                        <div class="column is-4">
+                            <h2 class="has-text-centered">Sociaux</h2>
                         </div>
-                        <div class="column is-vcentered">
-                            <h2>Mentaux</h2>
+                        <div class="column is-4">
+                            <h2 class="has-text-centered">Mentaux</h2>
                         </div>
                     </div>
                 </b-tab-item>
