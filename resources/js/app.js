14e320e3a1bdb42ae25396762f348c55ac8dab9a
Matt MARTIN <codingstuff91@gmail.com>
2021-10-11 22:39:20 +0200

    Ajout config Buefy

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,26 +6,13 @@
 
 require('./bootstrap');
 
-window.Vue = require('vue').default;
+Vue.component('vtm-v20-character-sheet', require('./components/VTM_V20_character_sheet.vue').default);
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
-
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
-
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+ import Vue from 'vue'
+ import Buefy from 'buefy'
+ import 'buefy/dist/buefy.css'
+ 
+ Vue.use(Buefy)
 
 const app = new Vue({
     el: '#app',
