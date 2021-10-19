779b498a327f90c2e6a60a694a3699e719e0d0c0
Matthieu MARTIN <codingstuff91@gmail.com>
2021-10-19 22:44:32 +0200

    Ajout methode updateDescription et deplacement des titres dans component

--- resources/js/components/EditDescription.vue
+++ resources/js/components/EditDescription.vue
@@ -1,10 +1,14 @@
 <template>
     <div>
+        <h1>Edition de description de caracteristique</h1>
+
+        <h2>Choix de la caracteristiques à éditer :</h2>
+
         <select name="" id="" v-model="caracteristique_id">
             <option v-for="item in caracteristiques" :key="item.id" :value="item.id">{{ item.nom }}</option>
         </select>
         <quill-editor ref="myQuillEditor" v-model="description"/>
-        <button @click="updateDescription">Update</button>
+        <button @click="updateDescription(caracteristique_id,description)">Update</button>
     </div>
 </template>
 
@@ -28,8 +32,15 @@ export default {
         getDescription(id) {
             axios.get('/description/' + id).then(response => this.description = response.data)
         },
-        updateDescription(){
-            alert('prout')
+        updateDescription(id, description){
+            axios.put('/description/'+ id +'/edit', {
+                description : this.description
+            }).then(response => {
+                this.$buefy.toast.open({
+                    message: response.data,
+                    type: 'is-success'
+                })
+            })
         }
     },
 }
--- resources/views/description/edit.blade.php
+++ resources/views/description/edit.blade.php
@@ -4,10 +4,6 @@
 
 <div class="container">
     
-    <h1>Edition de description de caracteristique</h1>
-
-    <h2>Choix de la caracteristiques à éditer :</h2>
-
     <edit-description :caracteristiques="{{ $caracteristiques }}"/>
 
 </div>
