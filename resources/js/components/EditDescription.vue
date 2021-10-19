9c69bce7506bcce0e734f18e6df310a30c20237f
Matthieu MARTIN <codingstuff91@gmail.com>
2021-10-19 23:13:08 +0200

    Mise en forme des styles du component

--- resources/js/components/EditDescription.vue
+++ resources/js/components/EditDescription.vue
@@ -1,14 +1,20 @@
 <template>
     <div>
-        <h1>Edition de description de caracteristique</h1>
+        <h1 class="has-text-centered is-size-3 mb-4">Edition de description de caracteristique</h1>
 
-        <h2>Choix de la caracteristiques à éditer :</h2>
+        <h2 class="has-text-centered is-size-5 my-4">Choix de la caracteristiques à éditer :</h2>
 
-        <select name="" id="" v-model="caracteristique_id">
-            <option v-for="item in caracteristiques" :key="item.id" :value="item.id">{{ item.nom }}</option>
-        </select>
-        <quill-editor ref="myQuillEditor" v-model="description"/>
-        <button @click="updateDescription(caracteristique_id,description)">Update</button>
+        <b-field label="Description à éditer" class="has-text-centered my-4">
+            <b-select placeholder="Choisir une caracteristiques" rounded v-model="caracteristique_id">
+                <option v-for="item in caracteristiques" :key="item.id" :value="item.id">{{ item.nom }}</option>
+            </b-select>
+        </b-field>
+
+        <div class="my-4">
+            <quill-editor v-model="description"/>
+        </div>
+
+        <b-button expanded class="mt-4" type="is-success" @click="updateDescription(caracteristique_id,description)">Mettre à jour</b-button>
     </div>
 </template>
 
