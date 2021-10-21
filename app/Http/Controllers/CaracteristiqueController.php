4c3ea97804a18771ffae79b9c18e9b5513ebf5f2
Matthieu MARTIN <codingstuff91@gmail.com>
2021-10-21 11:11:27 +0200

    Maj requete recuperation des niveaux

--- app/Http/Controllers/CaracteristiqueController.php
+++ app/Http/Controllers/CaracteristiqueController.php
@@ -9,14 +9,16 @@ use Illuminate\Support\Facades\DB;
 
 class CaracteristiqueController extends Controller
 {
-    public function get_caracteristiques_levels(Request $request)
+    public function get_levels(Request $request)
     {
         $caracteristiques = DB::table('caracteristiques')
         ->join('categories', 'caracteristiques.categorie_id', '=', 'categories.id')
         ->join('caracteristique_personnage', 'caracteristique_personnage.caracteristique_id', '=', 'caracteristiques.id')
-        ->select('categories.nom','caracteristiques.nom','caracteristique_personnage.niveau')
-        ->where('personnage_id', $request->personnage_id)
-        ->where('categories.nom', $request->categorie)
+        ->select('caracteristiques.id','caracteristiques.nom','caracteristique_personnage.niveau')
+        ->where('personnage_id', $request->character_id)
+        ->where('categories.nom', $request->characteristics_category_name)
         ->get();
+
+        return $caracteristiques;
     }
 }
