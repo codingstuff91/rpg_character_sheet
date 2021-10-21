317835079fac2a6f456c58c48ebf4a75aba03480
Matthieu MARTIN <codingstuff91@gmail.com>
2021-10-21 15:52:27 +0200

    Affichage des attributs dans composant dedié

--- resources/js/components/vtm_character.vue
+++ resources/js/components/vtm_character.vue
@@ -4,44 +4,7 @@
         <div class="row justify-content-center">
             <h2>Personnage : </h2>
             <b-tabs>
-                <b-tab-item label="Attributs / Capacités">
-                    <!-- Affichage des attributs -->
-                    <h1 class="has-text-centered is-size-4">Attributs</h1>
-                    <div class="columns is-mobile">
-                        <div class="column is-4">
-                            <h2 class="has-text-centered mb-4 is-size-5">Physiques</h2>
-                            <div class="my-4" v-for="attribut in attributs_physiques" :key="attribut.id">
-                                <h3 class="has-text-centered" @click="getDescription(attribut.id)">{{ attribut.nom }}</h3>
-                                <b-rate icon-pack="fas" v-model="attribut.niveau"></b-rate>
-                            </div>
-                        </div>
-                        <div class="column is-4">
-                            <h2 class="has-text-centered mb-4 is-size-5">Sociaux</h2>
-                            <div class="my-4" v-for="attribut in attributs_sociaux" :key="attribut.id">
-                                <h3 class="has-text-centered" @click="getDescription(attribut.id)">{{ attribut.nom }}</h3>
-                                <b-rate icon-pack="fas" v-model="attribut.niveau"></b-rate>
-                            </div>
-                        </div>
-                        <div class="column is-4">
-                            <h2 class="has-text-centered mb-4 is-size-5">Mentaux</h2>
-                            <div class="my-4" v-for="attribut in attributs_mentaux" :key="attribut.id">
-                                <h3 class="has-text-centered" @click="getDescription(attribut.id)">{{ attribut.nom }}</h3>
-                                <b-rate icon-pack="fas" v-model="attribut.niveau"></b-rate>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- Affichage des compétences -->
-                    <h1 class="has-text-centered is-size-4">Compétences</h1>
-                    <div class="columns is-mobile">
-                        <div class="column is-4">
-                            <h2 class="has-text-centered mb-4 is-size-5">Talents</h2>
-                            <div v-for="competence in competences_talents" :key="competence.id">
-                                <h3 class="has-text-centered" @click="getDescription(competence.id)">{{ competence.nom }}</h3>
-                                <b-rate icon-pack="fas" v-model="competence.niveau"></b-rate>
-                            </div>
-                        </div>
-                    </div>
-                </b-tab-item>
+                <Attributs :personnage="personnage"/>
 
                 <b-tab-item label="Avantages">
                     
@@ -70,15 +33,13 @@
 </template>
 
 <script>
-import Navbar from './Navbar.vue';
 import axios from 'axios';
+import Navbar from './Navbar.vue';
+import Attributs from './Attributs.vue'
 
     export default {
         data() {
             return {
-                attributs_physiques : [],
-                attributs_sociaux : [],
-                attributs_mentaux: [],
                 competences_talents : [],
                 isCardModalActive : false,
                 description_caracteristique : ''
@@ -86,6 +47,7 @@ import axios from 'axios';
         },
         components: {
             Navbar,
+            Attributs
         },
         props: {
             personnage: {
@@ -106,9 +68,7 @@ import axios from 'axios';
             }
         },
         async mounted(){
-            await this.getCaracteristiquesLevels(this.personnage.id, 'Attributs_physiques', 'attributs_physiques')
-            await this.getCaracteristiquesLevels(this.personnage.id, 'Attributs_mentaux', 'attributs_mentaux')
-            await this.getCaracteristiquesLevels(this.personnage.id, 'Attributs_sociaux', 'attributs_sociaux')
+
         }
     }
 </script>
