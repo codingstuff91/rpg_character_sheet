6dc2cc9e6bb0eff07b60b1ea1ef4797f7287b223
Matthieu MARTIN <codingstuff91@gmail.com>
2021-10-22 14:46:42 +0200

    Definition des relationship du model Jauge

--- app/Models/Jauge.php
+++ app/Models/Jauge.php
@@ -2,12 +2,24 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Personnage;
+use App\Models\Caracteristique;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Jauge extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
+
+    public function caracteristique()
+    {
+        return $this->hasOne(Caracteristique::class);
+    }
+
+    public function personnage()
+    {
+        return $this->hasOne(Personnage::class);
+    }
 }
