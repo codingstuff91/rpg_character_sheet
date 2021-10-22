1ea7b6acd875f56776a9248d194233ee6843d49a
Matthieu MARTIN <37110847+codingstuff91@users.noreply.github.com>
2021-10-22 10:17:26 +0200

    Ajout caractéristiques et valeurs pour infos générales personnage (#6)
    * Ajout capacites, attributs mentaux et sociaux et commentaires
    
    * Prise en compte ajout capacites et attributs sociaux
    
    * Ajout categorie infos générales et modif humanité
    
    * Ajout caracteristiques infos générales
    
    * Ajout valeur caracteristiques infos générales

--- database/seeders/Caracteristique_personnageSeeder.php
+++ database/seeders/Caracteristique_personnageSeeder.php
@@ -14,13 +14,57 @@ class Caracteristique_personnageSeeder extends Seeder
      */
     public function run()
     {
-        // Création de valeurs de niveau de caracteristiques aléatoires
-        for ($i=1; $i < 19 ; $i++) { 
-            DB::table('caracteristique_personnage')->insert([
-                "personnage_id" => 1,
-                "caracteristique_id" => $i,
-                "niveau" => rand(0,5)
-            ]);
-        }
-    }
-}
+        // Création de valeurs de caracteristique pour les infos générales
+        DB::table('caracteristique_personnage')->insert([
+            "personnage_id" => 1,
+            "caracteristique_id" => 1,
+            "niveau" => "Nosfé"
+        ]);
+
+        DB::table('caracteristique_personnage')->insert([
+            "personnage_id" => 1,
+            "caracteristique_id" => 2,
+            "niveau" => "Angers By night"
+        ]);
+
+        DB::table('caracteristique_personnage')->insert([
+            "personnage_id" => 1,
+            "caracteristique_id" => 3,
+            "niveau" => "Inconnu"
+        ]);
+
+        DB::table('caracteristique_personnage')->insert([
+            "personnage_id" => 1,
+            "caracteristique_id" => 4,
+            "niveau" => "Etudiant"
+        ]);
+
+        DB::table('caracteristique_personnage')->insert([
+            "personnage_id" => 1,
+            "caracteristique_id" => 5,
+            "niveau" => "Trouver son sire et le tuer"
+        ]);
+
+        DB::table('caracteristique_personnage')->insert([
+            "personnage_id" => 1,
+            "caracteristique_id" => 6,
+            "niveau" => "Trouver son sire et le tuer"
+        ]);
+
+        DB::table('caracteristique_personnage')->insert([
+            "personnage_id" => 1,
+            "caracteristique_id" => 7,
+            "niveau" => "Chasseur"
+        ]);
+
+        DB::table('caracteristique_personnage')->insert([
+            "personnage_id" => 1,
+            "caracteristique_id" => 8,
+            "niveau" => "Assamite"
+        ]);
+
+        DB::table('caracteristique_personnage')->insert([
+            "personnage_id" => 1,
+            "caracteristique_id" => 9,
+            "niveau" => 12
+        ]);
