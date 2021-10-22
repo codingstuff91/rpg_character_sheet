210ddc19aa471034c9e0623254b6d7f68655deea
Matthieu MARTIN <codingstuff91@gmail.com>
2021-10-22 15:38:26 +0200

    Modification complete requete recup niveaux de jauge

--- app/Http/Controllers/JaugeController.php
+++ app/Http/Controllers/JaugeController.php
@@ -2,15 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Jauge;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class JaugeController extends Controller
 {
     public function get_jauge_value(Request $request)
-    {
-        $value = Jauge::where('personnage_id', $request->personnage_id)
-                    ->where('caracteristique_id', $request->caracteristique)
-                    ->select('score', 'score_max')
-                    ->get();
+    {                    
+        $scores = DB::table('jauges')
+        ->join('caracteristiques', 'jauges.caracteristique_id', '=', 'caracteristiques.id')
+        ->where('personnage_id', $request->personnage_id)
+        ->where('caracteristiques.nom', $request->nom_caracteristique)
+        ->select('score', 'score_max')
+        ->get()->dd();
+
+        return $scores;
     }
 }
