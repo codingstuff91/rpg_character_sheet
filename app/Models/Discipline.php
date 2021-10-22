47dc1214dad408c77f02aa0893438d6ab1a13ecd
Matthieu MARTIN <codingstuff91@gmail.com>
2021-10-22 18:50:26 +0200

    Relation entre Personnage et Discipline

--- app/Models/Discipline.php
+++ app/Models/Discipline.php
@@ -2,12 +2,18 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Personnage;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Discipline extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
+
+    public function personnage()
+    {
+        return $this->belongsToMany(Personnage::class);
+    }
 }
