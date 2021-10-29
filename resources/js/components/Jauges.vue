564a37ae618e5a73d7f2ef0241741664e7bab1b1
Matthieu MARTIN <codingstuff91@gmail.com>
2021-10-29 17:01:34 +0200

    Ajout score humanité et import nouvelle icone

--- resources/js/components/Jauges.vue
+++ resources/js/components/Jauges.vue
@@ -36,15 +36,28 @@
             <b-rate icon-pack="fas"
                 :size="size"
                 :max="soif_max"
-                :icon="icon"
+                icon="tint"
                 spaced
                 v-model="soif">
+            </b-rate>
+        </div>
+        <div class="is-flex is-flex-direction-column is-justify-content-center is-align-items-center mt-4">
+            <h2 class="has-text-centered mb-4 is-size-5">Humanité</h2>
+            <b-rate icon-pack="fas"
+                :size="size"
+                :max="10"
+                show-score="true"
+                icon="user-alt"
+                spaced
+                disabled
+                v-model="humanite[0].niveau">
             </b-rate>
         </div>
     </b-tab-item>
 </template>
 <script>
 import axios from 'axios'
+import CaracteristiquesMixin from '../mixins/caracteristiquesMixin.vue'
 
     export default {
         data() {
@@ -56,8 +69,8 @@ import axios from 'axios'
                volonte_max : 6,
                volonte_actuelle : 6,
                soif_max : 5,
+               humanite : [],
                soif : 0,
-               icon : 'tint', 
                size : 'is-large'
             }
         },
@@ -66,6 +79,7 @@ import axios from 'axios'
                 type: Object
             },
         },
+        mixins: [CaracteristiquesMixin],
         methods: {
             getJaugeValue(personnage_id, caracteristique, data, data_max) {
                 axios.get(`/character/${personnage_id}/${caracteristique}/jauge_level`).then(response =>{
@@ -79,6 +93,7 @@ import axios from 'axios'
             await this.getJaugeValue(this.personnage.id, 'Volonté', 'volonte_actuelle', 'volonte_max')
             await this.getJaugeValue(this.personnage.id, 'Dégats Contondants', 'degats_contondants', 'degats_contondants_max')
             await this.getJaugeValue(this.personnage.id, 'Dégats Aggravés', 'degats_aggraves', 'degats_aggraves_max')
+            await this.getCaracteristiquesLevels(this.personnage.id, 'Humanité', 'humanite')
         },
     }
 </script>
