be1007d0b1dd1a5aa4f265da14fc5eeff9052e66
Matt MARTIN <codingstuff91@gmail.com>
2021-11-03 17:23:12 +0100

    Ajout valeurs pour les caracteristiques de sang

--- database/seeders/Caracteristique_personnageSeeder.php
+++ database/seeders/Caracteristique_personnageSeeder.php
@@ -93,5 +93,46 @@ class Caracteristique_personnageSeeder extends Seeder
             "niveau" => 2
         ]);
 
+        // Valeur aléatoire de coup de sang
+        DB::table('caracteristique_personnage')->insert([
+            "personnage_id" => 1,
+            "caracteristique_id" => Caracteristique::where('nom', 'Coup de sang')->get()->first()->id,
+            "niveau" => "+1 dé"
+        ]);
+
+        // Valeur aléatoire de coup de sang
+        DB::table('caracteristique_personnage')->insert([
+            "personnage_id" => 1,
+            "caracteristique_id" => Caracteristique::where('nom', 'Dégats régénérés')->get()->first()->id,
+            "niveau" => "1 dégat superficiel / tour"
+        ]);
+
+        // Valeur aléatoire de coup de sang
+        DB::table('caracteristique_personnage')->insert([
+            "personnage_id" => 1,
+            "caracteristique_id" => Caracteristique::where('nom', 'Bonus aux pouvoirs')->get()->first()->id,
+            "niveau" => "N/A"
+        ]);
+
+        // Valeur aléatoire de coup de sang
+        DB::table('caracteristique_personnage')->insert([
+            "personnage_id" => 1,
+            "caracteristique_id" => Caracteristique::where('nom', 'Test d\'exaltation')->get()->first()->id,
+            "niveau" => "1D10"
+        ]);
+
+        // Valeur aléatoire de coup de sang
+        DB::table('caracteristique_personnage')->insert([
+            "personnage_id" => 1,
+            "caracteristique_id" => Caracteristique::where('nom', 'Pénalité pour se nourrir')->get()->first()->id,
+            "niveau" => "N/A"
+        ]);
+
+        // Valeur aléatoire de coup de sang
+        DB::table('caracteristique_personnage')->insert([
+            "personnage_id" => 1,
+            "caracteristique_id" => Caracteristique::where('nom', 'Score de fléau')->get()->first()->id,
+            "niveau" => "1"
+        ]);
     }
 }
