8448df206f0c0cc1cb10f48f1b7498e6907220eb
Matt MARTIN <codingstuff91@gmail.com>
2021-11-03 19:25:24 +0100

    Ajout de seeder pour les infos generales

---
+++ database/seeders/InfosGeneralesSeeder.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class InfosGeneralesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('caracteristique_personnage')->insert([
+            "personnage_id" => 1,
+            "caracteristique_id" => Caracteristique::where('nom', 'Nom')->get()->first()->id,
+            "niveau" => "Nosfé"
+        ]);
+
+        DB::table('caracteristique_personnage')->insert([
+            "personnage_id" => 1,
+            "caracteristique_id" => Caracteristique::where('nom', 'Chronique')->get()->first()->id,
+            "niveau" => "Angers By Night"
+        ]);
+
+        DB::table('caracteristique_personnage')->insert([
+            "personnage_id" => 1,
+            "caracteristique_id" => Caracteristique::where('nom', 'Sire')->get()->first()->id,
+            "niveau" => "Inconnu"
+        ]);
+
+        DB::table('caracteristique_personnage')->insert([
+            "personnage_id" => 1,
+            "caracteristique_id" => Caracteristique::where('nom', 'Concept')->get()->first()->id,
+            "niveau" => "Etudiant"
+        ]);
+
+        DB::table('caracteristique_personnage')->insert([
+            "personnage_id" => 1,
+            "caracteristique_id" => Caracteristique::where('nom', 'Clan')->get()->first()->id,
+            "niveau" => "Assamite"
+        ]);
+
+        DB::table('caracteristique_personnage')->insert([
+            "personnage_id" => 1,
+            "caracteristique_id" => Caracteristique::where('nom', 'Génération')->get()->first()->id,
+            "niveau" => 12
+        ]);
+    }
+}
