8d3886b92fb04b822318b79fb78d2cdffaca02a2
Matt MARTIN <codingstuff91@gmail.com>
2021-11-03 21:46:36 +0100

    Suppression des attributs du seeder caracteristiques

--- database/seeders/CaracteristiqueSeeder.php
+++ database/seeders/CaracteristiqueSeeder.php
@@ -118,69 +118,6 @@ class CaracteristiqueSeeder extends Seeder
         ]);
         
         /**
-         * CAPACITES - TALENTS
-         */
-        Caracteristique::create([
-            'categorie_id' => Categorie::where('nom', 'Capacités_talents')->get()->first()->id,
-            'nom' => 'Athlétisme',
-            'description' => 'Texte à saisir',
-        ]);
-        
-        Caracteristique::create([
-            'categorie_id' => Categorie::where('nom', 'Capacités_talents')->get()->first()->id,
-            'nom' => 'Bagarre',
-            'description' => 'Texte à saisir',
-        ]);
-        
-        Caracteristique::create([
-            'categorie_id' => Categorie::where('nom', 'Capacités_talents')->get()->first()->id,
-            'nom' => 'Commandement',
-            'description' => 'Texte à saisir',
-        ]);
-        
-        /**
-         * CAPACITES - COMPETENCES
-         */
-        Caracteristique::create([
-            'categorie_id' => Categorie::where('nom', 'Capacités_compétences')->get()->first()->id,
-            'nom' => 'Animaux',
-            'description' => 'Texte à saisir',
-        ]);
-        
-        Caracteristique::create([
-            'categorie_id' => Categorie::where('nom', 'Capacités_compétences')->get()->first()->id,
-            'nom' => 'Armes à feu',
-            'description' => 'Texte à saisir',
-        ]);
-
-        Caracteristique::create([
-            'categorie_id' => Categorie::where('nom', 'Capacités_compétences')->get()->first()->id,
-            'nom' => 'Artisanats',
-            'description' => 'Texte à saisir',
-        ]);
-
-        /**
-         * CAPACITES - CONNAISSANCES
-         */
-        Caracteristique::create([
-            'categorie_id' => Categorie::where('nom', 'Capacités_connaissances')->get()->first()->id,
-            'nom' => 'Droit',
-            'description' => 'Texte à saisir',
-        ]);
-        
-        Caracteristique::create([
-            'categorie_id' => Categorie::where('nom', 'Capacités_connaissances')->get()->first()->id,
-            'nom' => 'Erudition',
-            'description' => 'Texte à saisir',
-        ]);
-        
-        Caracteristique::create([
-            'categorie_id' => Categorie::where('nom', 'Capacités_connaissances')->get()->first()->id,
-            'nom' => 'Finances',
-            'description' => 'Texte à saisir',
-        ]);
-
-        /**
          * JAUGES (Santé, Volonté, Soif et Humanité)
          */
         Caracteristique::create([
