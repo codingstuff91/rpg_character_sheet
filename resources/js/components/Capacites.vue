95617e7b2f5d18822522f817c4e1ebca0fe2520a
Matthieu MARTIN <37110847+codingstuff91@users.noreply.github.com>
2021-11-04 15:44:11 +0100

    ajout-valeurs-capacités-mentales (#25)
    * Affichage de toutes les capacités
    
    * Ajout valeurs de capacites mentales

--- resources/js/components/Capacites.vue
+++ resources/js/components/Capacites.vue
@@ -1,11 +1,37 @@
 <template>
     <b-tab-item label="Capacités">
         <!-- Affichage des compétences -->
-        <h1 class="has-text-centered is-size-4">Compétences</h1>
+        <h1 class="has-text-centered is-size-4">Capacités</h1>
         <div class="columns is-mobile">
             <div class="column is-4">
-                <h2 class="has-text-centered mb-4 is-size-5">Talents</h2>
+                <h2 class="has-text-centered mb-4 is-size-5">Physiques</h2>
                 <div :class="box_style" v-for="capacite in capacites_talents" :key="capacite.id">
+                    <h3 class="has-text-centered" @click="getDescription(capacite.id)">{{ capacite.nom }}</h3>
+                    <b-rate 
+                        icon-pack="fas" 
+                        :icon="icon"
+                        spaced
+                        :size="size"
+                        v-model="capacite.niveau">
+                    </b-rate>
+                </div>
+            </div>
+            <div class="column is-4">
+                <h2 class="has-text-centered mb-4 is-size-5">Sociales</h2>
+                <div :class="box_style" v-for="capacite in capacites_competences" :key="capacite.id">
+                    <h3 class="has-text-centered" @click="getDescription(capacite.id)">{{ capacite.nom }}</h3>
+                    <b-rate 
+                        icon-pack="fas" 
+                        :icon="icon"
+                        spaced
+                        :size="size"
+                        v-model="capacite.niveau">
+                    </b-rate>
+                </div>
+            </div>
+            <div class="column is-4">
+                <h2 class="has-text-centered mb-4 is-size-5">Mentales</h2>
+                <div :class="box_style" v-for="capacite in capacites_connaissances" :key="capacite.id">
                     <h3 class="has-text-centered" @click="getDescription(capacite.id)">{{ capacite.nom }}</h3>
                     <b-rate 
                         icon-pack="fas" 
@@ -42,6 +68,8 @@ import CaracteristiquesMixin from '../mixins/caracteristiquesMixin.vue'
         mixins: [CaracteristiquesMixin],
         async mounted(){
             await this.getCaracteristiquesLevels(this.personnage.id, 'Capacités_talents', 'capacites_talents')
+            await this.getCaracteristiquesLevels(this.personnage.id, 'Capacités_compétences', 'capacites_competences')
+            await this.getCaracteristiquesLevels(this.personnage.id, 'Capacités_connaissances', 'capacites_connaissances')
         }
     }
 </script>
