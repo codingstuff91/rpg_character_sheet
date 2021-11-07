168d225c14fd7a4158ba23ecd0361ce35e1d8db4
Matthieu MARTIN <codingstuff91@gmail.com>
2021-11-07 10:27:38 +0100

    Mise a jour de la jauge de volonté

--- resources/js/components/Volonte.vue
+++ resources/js/components/Volonte.vue
@@ -10,11 +10,12 @@
                 <div class="is-flex is-flex-direction-column is-justify-content-center is-align-items-center my-4">
                     <b-rate 
                         icon-pack="fas" 
-                        :max="degats_contondants_max"
+                        :max="volonte_max"
                         icon='fist-raised'
                         spaced
                         :size="size"
-                        v-model="degats_contondants">
+                        @change="updateJaugeVolonte"
+                        v-model="volonte">
                     </b-rate>
                     
                     <b-notification
@@ -46,10 +47,9 @@ import CaracteristiquesMixin from '../mixins/caracteristiquesMixin.vue'
     export default {
         data() {
             return {
-               degats_contondants : 7,
-               degats_contondants_max : 10,
-               degats_aggraves : 2, 
-               degats_aggraves_max : 7,
+               volonte : 7,
+               volonte_max : 10,
+               jauge_volonte_id : 0, 
                size : 'is-large'
             }
         },
@@ -60,19 +60,29 @@ import CaracteristiquesMixin from '../mixins/caracteristiquesMixin.vue'
         },
         mixins: [CaracteristiquesMixin],
         methods: {
-            getJaugeValue(personnage_id, caracteristique, data, data_max) {
+            getJaugeValue(personnage_id, caracteristique, data, data_max, jauge_id) {
                 axios.get(`/character/${personnage_id}/${caracteristique}/jauge_level`).then(response =>{
                     this[data] = response.data[0].score
                     this[data_max] = response.data[0].score_max
+                    this[jauge_id] = response.data[0].caracteristique_id
                 })
             },
-            getDescription(caracteristique_id){
-                this.$emit('get_description',caracteristique_id)
+            updateJaugeVolonte(new_value){
+                axios.post(`/character/${this.personnage.id}/${this.jauge_volonte_id}/update_jauge_level`, {
+                    new_value
+                }).then(response => {
+                    console.log(response.data);
+                }).catch(error => {
+                    console.log(error);
+                    this.$buefy.toast.open({
+                        message: "Erreur lors de la de mise à jour",
+                        type: 'is-danger'
+                    })                    
+                })
             }
         },
         async mounted() {
-            await this.getJaugeValue(this.personnage.id, 'Dégats Contondants', 'degats_contondants', 'degats_contondants_max')
-            await this.getJaugeValue(this.personnage.id, 'Dégats Aggravés', 'degats_aggraves', 'degats_aggraves_max')
+            await this.getJaugeValue(this.personnage.id, 'Volonté', 'volonte', 'volonte_max', 'jauge_volonte_id')
         },
     }
 </script>
