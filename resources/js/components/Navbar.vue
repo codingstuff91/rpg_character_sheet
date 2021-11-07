b6dde8a24edb737ea422f0092e64f81457f4da9e
Matt MARTIN <codingstuff91@gmail.com>
2021-11-07 19:13:49 +0100

    Suppression liens inutiles

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -12,31 +12,7 @@
         </template>
         <template #start>
             <b-navbar-item href="#">
-                Home
-            </b-navbar-item>
-            <b-navbar-item href="#">
-                Documentation
-            </b-navbar-item>
-            <b-navbar-dropdown label="Info">
-                <b-navbar-item href="#">
-                    About
-                </b-navbar-item>
-                <b-navbar-item href="#">
-                    Contact
-                </b-navbar-item>
-            </b-navbar-dropdown>
-        </template>
-
-        <template #end>
-            <b-navbar-item tag="div">
-                <div class="buttons">
-                    <a class="button is-primary">
-                        <strong>Sign up</strong>
-                    </a>
-                    <a class="button is-light">
-                        Log in
-                    </a>
-                </div>
+                Accueil
             </b-navbar-item>
         </template>
     </b-navbar>
