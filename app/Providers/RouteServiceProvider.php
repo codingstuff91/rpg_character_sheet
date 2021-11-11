a38d9265fa8fea69a2b93ec1d501ade16115d9bf
Matt MARTIN <codingstuff91@gmail.com>
2021-11-11 19:50:59 +0100

    Change route after login and register

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/character/1';
+    public const HOME = '/';
 
     /**
      * The controller namespace for the application.
