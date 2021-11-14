0ba650039063e4d5d3b7e5e9cb8da187ba6acf1a
Matt MARTIN <codingstuff91@gmail.com>
2021-11-14 19:09:55 +0100

    Add create and store methods

--- app/Http/Controllers/DisciplineController.php
+++ app/Http/Controllers/DisciplineController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Discipline;
 use App\Models\Personnage;
 use Illuminate\Http\Request;
 
@@ -12,5 +13,35 @@ class DisciplineController extends Controller
         $personnage = Personnage::find($request->personnage_id);
 
         return $personnage->disciplines()->get()->groupBy('nom');
+    }
+
+    public function create()
+    {
+        return view('discipline.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nom' => 'required',
+            'niveau' => 'required',
+            'pouvoir' => 'required',
+            'description' => 'required',
+            'cout' => 'required',
+            'groupement_des' => 'required',
+            'systeme' => 'required',
+            'duree' => 'required',
+        ]);
+        
+        Discipline::create([
+            "nom" => $request->discipline['nom'],
+            "niveau" => $request->discipline['niveau'],
+            "pouvoir" => $request->discipline['pouvoir'],
+            "description" => $request->discipline['description'],
+            "cout" => $request->discipline['cout'],
+            "groupement_des" => $request->discipline['groupement_des'],
+            "systeme" => $request->discipline['systeme'],
+            "duree" => $request->discipline['duree']
+        ]);
     }
 }
