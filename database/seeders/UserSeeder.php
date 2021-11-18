9bb733d76f4a935bd8d32d318fa20047fd272a4b
Matthieu MARTIN <codingstuff91@gmail.com>
2021-11-18 10:59:53 +0100

    Update UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,9 +17,9 @@ class UserSeeder extends Seeder
     {     
         User::create([
             'name' => 'Dimitri',
-            'email' => 'dimitri.krajka@gmail.com',
+            'email' => 'dimitrikrajka@hotmail.fr',
             'role' => 'joueur',
-            'password' => bcrypt('secret'),
+            'password' => bcrypt('krajka_secret'),
             'remember_token' => Str::random(20)
         ]);
 
@@ -27,31 +27,31 @@ class UserSeeder extends Seeder
             'name' => 'Déborah',
             'email' => 'Deborah.capaldi@hotmail.fr',
             'role' => 'joueur',
-            'password' => bcrypt('secret'),
+            'password' => bcrypt('capaldi_secret'),
             'remember_token' => Str::random(20)
         ]);
 
         User::create([
             'name' => 'Mael',
-            'email' => 'mael@gmail.com',
+            'email' => 'dauphinmael@gmail.com',
             'role' => 'joueur',
-            'password' => bcrypt('secret'),
+            'password' => bcrypt('dauphin_secret'),
             'remember_token' => Str::random(20)
         ]);
 
         User::create([
             'name' => 'Bellal',
-            'email' => 'bellal@gmail.com',
+            'email' => 'bellal.waissi@gmail.com',
             'role' => 'joueur',
-            'password' => bcrypt('secret'),
+            'password' => bcrypt('waissi_secret'),
             'remember_token' => Str::random(20)
         ]);
 
         User::create([
             'name' => 'Martin',
-            'email' => 'martin@gmail.com',
+            'email' => 'm_truffier@hotmail.fr',
             'role' => 'joueur',
-            'password' => bcrypt('secret'),
+            'password' => bcrypt('truffier_secret'),
             'remember_token' => Str::random(20)
         ]);
 
