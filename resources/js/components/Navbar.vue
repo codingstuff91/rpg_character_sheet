76fd91f01caca4212db1b53b9d78241af550b141
Matthieu MARTIN <codingstuff91@gmail.com>
2021-11-20 14:15:28 +0100

    Add navlinks

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -6,6 +6,18 @@
                 <h3 class="title">Vamp Perso</h3>
             </b-navbar-item>
         </template>
+        <template #end>
+            <b-navbar-item tag="div">
+                <div>
+                    <a href="/" class="button">
+                        <strong>Accueil</strong>
+                    </a>
+                    <a href="/character/all" class="button is-danger">
+                        <strong>Personnages</strong>
+                    </a>
+                </div>
+            </b-navbar-item>
+        </template>
     </b-navbar>
 </template>
 
