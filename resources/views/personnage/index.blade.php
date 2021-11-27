0b2cef881350e80af5eab10c995b4bbc41c01268
Matt MARTIN <codingstuff91@gmail.com>
2021-11-27 19:16:11 +0100

    Modif affichage des personnages. Ajout icone vampire et nom de chaque joueur Ajout verification affichage lien en fonction ID du joueur

--- resources/views/personnage/index.blade.php
+++ resources/views/personnage/index.blade.php
@@ -8,14 +8,22 @@
             @foreach ($personnages as $perso)
 
                 <div class="column is-1/4">
-                    <article class="message is-danger">
-                        <div class="message-header">
-                        <p>{{ $perso->nom }}</p>
-                        </div>
-                        <div class="message-body">
-                            <a href="/character/{{ $perso->id }}">Feuille personnage</a>
-                        </div>
-                    </article>
+                    <div class="box is-flex is-flex-direction-column ml-2">
+                        <h3 class="is-size-5 my-2 has-text-weight-bold has-text-centered">
+                            <span class="icon">
+                                <img src="{{ asset('img/vampire.png') }}">
+                            </span>
+                            {{ $perso->nom }}
+                        </h3>
+                        <h4 class="is-size-6 mb-2 has-text-centered">
+                            <span class="icon">
+                                <i class="fas fa-user"></i>
+                            </span>
+                            Joueur : {{ $perso->user->name }}</h4>
+                        @if ($perso->user_id == auth()->user()->id || auth()->user()->role == "mj")
+                            <a href="/character/{{ $perso->id }}" class="button is-danger is-rounded is-small">Fiche perso</a>
+                        @endif
+                    </div>
                 </div>
     
             @endforeach
