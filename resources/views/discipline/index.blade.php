4dbc96a450848f83ef1f3a8afae1f1ee13eb62b0
Matthieu MARTIN <codingstuff91@gmail.com>
2021-12-05 14:29:25 +0100

    Ajout colonne niveau

--- resources/views/discipline/index.blade.php
+++ resources/views/discipline/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <div class="container">
-    <h1 class="title mt-4 text-center">Liste des disciplines</h1>
+    <h1 class="title mt-4 text-center is-size-1">Liste des disciplines</h1>
 
     <div class="is-flex is-justify-content-center">
         <a href="{{ route('discipline.create') }}" class="button is-success my-2">Ajouter une discipline</a>
@@ -15,6 +15,7 @@
                 <th>EDITER</th>
                 <th>NOM</th>
                 <th>POUVOIR</th>
+                <th>NIVEAU</th>
             </tr>
         </thead>
         <tbody>
@@ -24,6 +25,7 @@
                     <td><a href="{{ route('discipline.edit', $discipline->id) }}">Editer</a></td>
                     <td>{{ $discipline->nom }}</td>
                     <td>{{ $discipline->pouvoir }}</td>
+                    <td>{{ $discipline->niveau }}</td>
                 </tr>
             @endforeach
 
