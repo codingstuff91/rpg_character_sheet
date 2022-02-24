7c933b644569f97070c9f7c47e9e2561d5d77c10
Matt MARTIN <codingstuff91@gmail.com>
2022-02-24 22:42:24 +0100

    Génération des résultats de dés et dés de soif

--- app/Http/Controllers/PersonnageController.php
+++ app/Http/Controllers/PersonnageController.php
@@ -32,4 +32,34 @@ class PersonnageController extends Controller
 
         return view('personnage.show')->with(['personnage' => $personnage]);
     }
+    
+    /**
+     * Générer un lancer de dés 10
+     * 
+     * @param Integer $nombre_des Le nombre de dés classiques à lancer
+     * @param Integer $nombre_des_soif le nombre de dés de soif à lancer
+     * 
+     * @return Array
+     */
+    public function lancer_des(Request $request)
+    {
+        // return $request->all();
+        $resultats_lancer_des = [];
+        $resultats_lancer_des_soif = [];
+
+        if($request->nombre_des_soif != 0)
+        {
+            for ($i=0; $i < $request->nombre_des_soif ; $i++) { 
+                $resultat_des_soif = rand(1,10);
+                $resultats_lancer_des_soif[] = $resultat_des_soif;
+            }
+        }
+
+        for ($j=0; $j < $request->nombre_des - $request->nombre_des_soif ; $j++) { 
+            $resultat_des = rand(1,10);
+            $resultats_lancer_des[] = $resultat_des;
+        }
+
+        return [$resultats_lancer_des_soif, $resultats_lancer_des];
+    }
 }
