d4eed22206151fe2c50c18ba492d9301df919527
Matt MARTIN <codingstuff91@gmail.com>
2022-03-12 21:15:39 +0100

    Modification des messages par des onglets

--- resources/js/components/Disciplines.vue
+++ resources/js/components/Disciplines.vue
@@ -17,6 +17,41 @@
                 </div>
             </div>
         </b-modal>
+        <b-collapse
+            class="panel"
+            animation="slide"
+            v-for="(discipline,index) in disciplines" :key="index"
+            :open="isOpen">
+            <template #trigger>
+                <div
+                    class="panel-heading is-flex is-justify-content-space-between is-align-items-center"
+                    role="button"
+                    aria-controls="contentIdForA11y2">
+                    <strong>{{ discipline[0].nom }}</strong>
+                    <img src="/img/bloody.png">                    
+                </div>
+            </template>
+            <div class="panel-block is-flex is-flex-direction-column">
+                <div class="my-4" v-for="pouvoir in discipline" :key="pouvoir.nom">
+                    <h2 class="title is-size-5">{{pouvoir.pouvoir}} (niveau {{pouvoir.niveau}})</h2>
+                    <p class="is-size-6 my-2"><span class="has-text-weight-bold">Coût : </span>{{pouvoir.cout}}</p>
+                    <p class="is-size-6 my-2"><span class="has-text-weight-bold">Groupement de dès : </span>{{pouvoir.groupement_des}}</p>
+                    <p class="is-size-6 my-2"><span class="has-text-weight-bold">Systeme : </span> {{pouvoir.systeme}}</p>
+                    <p class="is-size-6 my-2"><span class="has-text-weight-bold">Durée : </span>{{pouvoir.duree}}</p>
+                    
+                    <p class="is-size-6 my-2">
+                        <b-button 
+                        type="is-danger"
+                        class="mt-2" 
+                        @click="displayDisciplineDescription(pouvoir.description)"
+                        rounded
+                        size="is-small">
+                            Description complète
+                        </b-button>
+                    </p>
+                </div>
+            </div>
+        </b-collapse>    
         <b-message 
             v-for="(discipline,index) in disciplines" :key="index"
             :title="discipline[0].nom" 
